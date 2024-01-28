089d5c1a7959c66f9ff224720045fc973f8efa72
baglana <102351104+baglana@users.noreply.github.com>
2024-01-28 20:02:02 +0600

    Add error handling

--- refreshQuantity.js
+++ refreshQuantity.js
@@ -10,7 +10,7 @@ const WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbx1yfGYp5C4r7-Fku_vl
  */
 function getRequestOptions_(method, access_token = ''){
   const payload = JSON.stringify({ //тело запроса
-    method, 
+    method,
     access_token
   })
 
@@ -34,34 +34,42 @@ function getAvailableMethods(){
   Logger.log(response)
 }
 
+
+
 function getAccessToken() {
-  const requestOptions = getRequestOptions_('getAccessToken')
-  const response = UrlFetchApp.fetch(WEBAPP_URL, requestOptions)
+  try {
+    const requestOptions = getRequestOptions_('getAccessToken')
+    const response = UrlFetchApp.fetch(WEBAPP_URL, requestOptions)
+    const data = JSON.parse(response.getContentText());
 
-  const data = JSON.parse(response.getContentText());
+    if (data?.status !== 'ok') {
+      throw new Error('error fetching access token: ' + data?.error);
+    }
 
-  if (data?.status !== 'ok') {
-    throw Error(data.error);
-    // throw Error('error getting access token');
+    return data?.access_token;
+    
+  } catch (err) {
+    throw err;
   }
-
-  
-  return data?.access_token;
 }
 
+
+
 function getAvailableItems() {
-  const requestOptions = getRequestOptions_('getAvailableItems', getAccessToken())
-  const response = UrlFetchApp.fetch(WEBAPP_URL, requestOptions)
-  
-  const data = JSON.parse(response.getContentText());
+  try {
+    const requestOptions = getRequestOptions_('getAvailableItems', getAccessToken())
+    const response = UrlFetchApp.fetch(WEBAPP_URL, requestOptions)
+    const data = JSON.parse(response.getContentText());
 
-  if (data?.status !== 'ok') {
-    throw Error(data.error);
-    // throw Error('error getting available items');
+    if (data?.status !== 'ok') {
+      throw new Error('error fetching available items: ' + data?.error);
+    }
+
+    return data?.items;
+
+  } catch (err) {
+    throw err;
   }
-
-
-  return data.items;
 }
 
 
@@ -75,40 +83,46 @@ function refreshQuantity(){
    * Напишите решение здесь
    */
 
-  // ss.toast('Произошла ошибка!')
+  try {
+    const availableItems = getAvailableItems();
+    if (!availableItems || !availableItems.length) {
+      return;
+    }
 
-  const availableItems = getAvailableItems();
-  if (!availableItems || !availableItems.length) {
-    return;
-  }
+    const stockSheet = ss.getSheetByName('Ассортимент');
+    const stockRange = stockSheet.getDataRange();
+    const stockValues = stockRange.getValues();
 
-  const stockSheet = ss.getSheetByName('Ассортимент');
-  const stockRange = ss.getDataRange();
-  const stockValues = stockRange.getValues();
+    const ID_COLUMN = 0;
+    const QUANTITY_COLUMN = 3;
 
-  const ID_COLUMN = 0;
-  const QUANTITY_COLUMN = 3;
+    for (let row = 1; row < stockValues.length; row++) {
+      const itemId = stockValues[row][ID_COLUMN];
 
-  for (let row = 1; row < stockValues.length; row++) {
-    const itemId = stockValues[row][ID_COLUMN];
+      let availableItem;
 
-    let availableItem;
+      if (itemId !== '') {
+        availableItem = availableItems.find((e) => {
+          return e.id === itemId;
+        });
 
-    if (itemId !== '') {
-      availableItem = availableItems.find((e) => {
-        return e.id === itemId;
-      });
-
-      if (availableItem) {
-        stockValues[row][QUANTITY_COLUMN] = availableItem.quantity;
+        if (availableItem) {
+          stockValues[row][QUANTITY_COLUMN] = availableItem.quantity;
+        }
       }
     }
-  }
 
-  stockRange.setValues(stockValues);
-  
-  ss.toast('Обновление завершено!')
+    stockRange.setValues(stockValues);
+    
+    ss.toast('Обновление завершено!')
+
+  } catch (err) {
+    ss.toast('Произошла ошибка! ' + err.message)
+    Logger.log(err);
+  }
 }
+
+
 
 /**
  * Простой триггер
